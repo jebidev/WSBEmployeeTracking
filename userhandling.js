c90992a886d3dff38fc10f8d280c01d8a8b9a1c2
TheAshb <ashwinbadal@rocketmail.com>
2024-04-19 23:39:33 -0500

    Added login functionality

--- userhandling.js
+++ userhandling.js
@@ -32,7 +32,7 @@ close_registration.addEventListener('click', function() {
   registration_modal.classList.remove('is-active');
 });
 
-  // Register an account
+// Register an account
 document.getElementById('create_account_button').addEventListener('click', function() {
   const email = document.getElementById('registration_email').value;
   const password = document.getElementById('registration_password').value;
@@ -43,5 +43,30 @@ document.getElementById('create_account_button').addEventListener('click', funct
     })
     .catch((error) => {
       console.error("Error creating user:", error.message);
+    });
+});
+
+// // Login an account
+// login_submit_button.addEventListener('click', function() {
+//   const email = document.getElementById('login_email').value;
+//   const password = document.getElementById('login_password').value;
+//   firebase.auth().signInWithEmailAndPassword(email, password)
+//     .then((userCredential) => {
+//       console.log("User logged in:", userCredential.user);
+//       login_modal.classList.remove('is-active');
+//     })
+//     .catch((error) => {
+//       console.error("Error logging in:", error.message);
+//     });
+// });
+
+const modal_signin = document.getElementById('modal_signin');
+
+modal_signin.addEventListener('click', function() {
+  const login_email = document.getElementById('login_email').value;
+  const login_password = document.getElementById('login_password').value;
+  firebase.auth().signInWithEmailAndPassword(login_email, login_password)
+    .then((userCredential) => {
+      console.log("User logged in:", userCredential.user);
     });
 });
